b38111c2c140cefce7ea79b0942811c5702f4988
Abderrahmane Tebbal <abdoutebbal444@gmail.com>
2026-03-06 17:17:24 +0100

    Create Values_and_Data_Types.py

---
+++ course1_python_basics/course1_python_basics/course1_python_basics/variables/Values_and_Data_Types.py
@@ -0,0 +1,21 @@
+print(10)
+#function_name(argument)
+#print() : is a function , 10 is a expression has one value which has a type , print(10) : is a function call
+# Python Values and Data Types
+
+| Data Type | Example             |
+| --------- | ------------------- |
+| int       | `10`                |
+| float     | `3.14`              |
+| complex   | `2+3j`              |
+| str       | `"hello"`           |
+| bool      | `True`              |
+| list      | `[1,2,3]`           |
+| tuple     | `(1,2)`             |
+| set       | `{1,2,3}`           |
+| dict      | `{"name":"Denver"}` |
+| NoneType  | `None`              |
+
+---
+
+These data types form the foundation of Python programming and are used to represent different kinds of information in a program.
